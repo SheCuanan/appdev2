b2cfe17315c202b0c5a40cdd7e1be1b446fb2a92
SheCuanan <160637669+SheCuanan@users.noreply.github.com>
2025-02-20 01:39:51 +0800

    Delete readReflection.js

--- readReflection.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('reflection.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
-    }
-    console.log(data);
-});
